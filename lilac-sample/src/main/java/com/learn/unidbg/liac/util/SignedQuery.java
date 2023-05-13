b8d1e5e17d507cc47118ecbfbb8e325da26324b5
geekrun <11956884+geekrun@users.noreply.github.com>
2023-05-13 13:00:46 +0800

    增加 bili

---
+++ lilac-sample/src/main/java/com/learn/unidbg/liac/util/SignedQuery.java
@@ -0,0 +1,127 @@
+package com.learn.unidbg.liac.util;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/* compiled from: BL */
+/* loaded from: classes.dex */
+public final class SignedQuery {
+
+    /* renamed from: c */
+    private static final char[] f106300c = "0123456789ABCDEF".toCharArray();
+
+    public static final String FIELD_DELIMITER = "&";
+    public static final String KEY_VALUE_DELIMITER = "=";
+
+
+    /* renamed from: a */
+    public final String f106301a;
+
+    /* renamed from: b */
+    public final String f106302b;
+
+    public SignedQuery(String str, String str2) {
+        this.f106301a = str;
+        this.f106302b = str2;
+    }
+
+    /* renamed from: a */
+    private static boolean m79205a(char c, String str) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || !((c < '0' || c > '9') && "-_.~".indexOf(c) == -1 && (str == null || str.indexOf(c) == -1));
+    }
+
+    /* renamed from: b */
+    static String m79204b(String str) {
+        return m79203c(str, null);
+    }
+
+    /* renamed from: c */
+    static String m79203c(String str, String str2) {
+        StringBuilder sb = null;
+        if (str == null) {
+            return null;
+        }
+        int length = str.length();
+        int i = 0;
+        while (i < length) {
+            int i2 = i;
+            while (i2 < length && m79205a(str.charAt(i2), str2)) {
+                i2++;
+            }
+            if (i2 == length) {
+                if (i == 0) {
+                    return str;
+                }
+                sb.append((CharSequence) str, i, length);
+                return sb.toString();
+            }
+            if (sb == null) {
+                sb = new StringBuilder();
+            }
+            if (i2 > i) {
+                sb.append((CharSequence) str, i, i2);
+            }
+            i = i2 + 1;
+            while (i < length && !m79205a(str.charAt(i), str2)) {
+                i++;
+            }
+            try {
+                byte[] bytes = str.substring(i2, i).getBytes("UTF-8");
+                int length2 = bytes.length;
+                for (int i3 = 0; i3 < length2; i3++) {
+                    sb.append('%');
+                    sb.append(f106300c[(bytes[i3] & 240) >> 4]);
+                    sb.append(f106300c[bytes[i3] & 15]);
+                }
+            } catch (UnsupportedEncodingException e) {
+                throw new AssertionError(e);
+            }
+        }
+        return sb == null ? str : sb.toString();
+    }
+
+
+
+
+    /* renamed from: r */
+    public static String m79203c(Map<String, String> map) {
+        if (!(map instanceof SortedMap)) {
+            map = new TreeMap(map);
+        }
+        StringBuilder sb = new StringBuilder(256);
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            if (!StringUtils.isEmpty(key)) {
+                sb.append(m79204b(key));
+                sb.append(KEY_VALUE_DELIMITER);
+                String value = entry.getValue();
+                sb.append(value == null ? "" : m79204b(value));
+                sb.append(FIELD_DELIMITER);
+            }
+        }
+        int length = sb.length();
+        if (length > 0) {
+            sb.deleteCharAt(length - 1);
+        }
+        if (length == 0) {
+            return null;
+        }
+        return sb.toString();
+    }
+
+    public String toString() {
+        String str = this.f106301a;
+        if (str == null) {
+            return "";
+        }
+        if (this.f106302b == null) {
+            return str;
+        }
+        return this.f106301a + "&sign=" + this.f106302b;
+    }
+}
---
+++ lilac-sample/src/main/java/com/learn/unidbg/liac/zsxq/Zuiyou.java
@@ -0,0 +1,137 @@
+package com.learn.unidbg.liac.zsxq;
+
+
+/*
+    public static native String sign(String str, byte[] bArr);
+
+function callSign(){
+  Java.perform(function() {
+    function stringToBytes(str) {
+      var javaString = Java.use('java.lang.String');
+      return javaString.$new(str).getBytes();
+    }
+
+    let NetCrypto = Java.use("com.izuiyou.network.NetCrypto");
+    let arg1 = "hello world";
+    let arg2 = "V I 50";
+    let ret = NetCrypto.sign(arg1, stringToBytes(arg2));
+    console.log("ret:"+ret);
+  })
+}
+
+ */
+
+import com.github.unidbg.AndroidEmulator;
+import com.github.unidbg.linux.android.AndroidEmulatorBuilder;
+import com.github.unidbg.linux.android.AndroidResolver;
+import com.github.unidbg.linux.android.dvm.*;
+import com.github.unidbg.memory.Memory;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+public class Zuiyou extends AbstractJni {
+    AndroidEmulator androidEmulator;
+
+    VM vm = null;
+
+
+    Memory memory = null;
+    DalvikModule dm;
+
+    private void run() {
+
+        androidEmulator = AndroidEmulatorBuilder.for32Bit().setProcessName("cn.xiaochuankeji.tieba").build();
+
+        vm = androidEmulator.createDalvikVM(new File("lilac-sample/src/main/resources/right573.apk"));
+        vm.setJni(this);
+        vm.setVerbose(true);
+        memory = androidEmulator.getMemory();
+        memory.setLibraryResolver(new AndroidResolver(23));
+        dm = vm.loadLibrary("net_crypto", true);
+        dm.callJNI_OnLoad(androidEmulator);
+
+
+        DvmClass dvmClass = vm.resolveClass("com.izuiyou.network.NetCrypto");
+        String arg1 = "hello world";
+        byte[] arg2 = "V I 50".getBytes(StandardCharsets.UTF_8);
+        String result = dvmClass.callStaticJniMethodObject(androidEmulator, "sign(Ljava/lang/String;[B)Ljava/lang/String;", arg1, arg2).getValue().toString();
+
+        System.out.printf(String.format("result======= %s", result));
+
+
+    }
+
+    @Override
+    public DvmObject<?> callObjectMethodV(BaseVM vm, DvmObject<?> dvmObject, String signature, VaList vaList) {
+        switch (signature) {
+            case "cn/xiaochuankeji/tieba/AppController->getPackageManager()Landroid/content/pm/PackageManager;": {
+                return vm.resolveClass("android/content/pm/PackageManager").newObject(null);
+            }
+
+            case "cn/xiaochuankeji/tieba/AppController->getPackageName()Ljava/lang/String;": {
+                String packageName = vm.getPackageName();
+                if (packageName != null) {
+                    return new StringObject(vm, packageName);
+                }
+                break;
+            }
+            case "cn/xiaochuankeji/tieba/AppController->getClass()Ljava/lang/Class;":
+                return dvmObject.getObjectType();
+            case "java/lang/Class->getSimpleName()Ljava/lang/String;":
+                return new StringObject(vm, ((DvmClass) dvmObject).getName());
+
+            case "cn/xiaochuankeji/tieba/AppController->getFilesDir()Ljava/io/File;": {
+                return vm.resolveClass("java/io/File").newObject("/data/data/cn.xiaochuankeji.tieba/files");
+            }
+            case "java/io/File->getAbsolutePath()Ljava/lang/String;": {
+                return new StringObject(vm, dvmObject.getValue().toString());
+            }
+        }
+        return super.callObjectMethodV(vm, dvmObject, signature, vaList);
+    }
+
+
+    @Override
+    public DvmObject<?> callStaticObjectMethodV(BaseVM vm, DvmClass dvmClass, String signature, VaList vaList) {
+        switch (signature) {
+            case "com/izuiyou/common/base/BaseApplication->getAppContext()Landroid/content/Context;": {
+                DvmObject<?> context = vm.resolveClass("cn/xiaochuankeji/tieba/AppController").newObject(null);
+                return context;
+            }
+        }
+        return super.callStaticObjectMethodV(vm, dvmClass, signature, vaList);
+    }
+
+
+    @Override
+    public boolean callStaticBooleanMethodV(BaseVM vm, DvmClass dvmClass, String signature, VaList vaList) {
+        switch (signature) {
+            case "android/os/Debug->isDebuggerConnected()Z": {
+                return false;
+            }
+        }
+        return super.callStaticBooleanMethodV(vm, dvmClass, signature, vaList);
+    }
+
+
+    @Override
+    public int callStaticIntMethodV(BaseVM vm, DvmClass dvmClass, String signature, VaList vaList) {
+
+        switch (signature) {
+            case "android/os/Process->myPid()I":
+                return androidEmulator.getPid();
+        }
+
+
+        return super.callStaticIntMethodV(vm, dvmClass, signature, vaList);
+    }
+
+
+    public static void main(String[] args) {
+
+        new Zuiyou().run();
+    }
+
+
+}
